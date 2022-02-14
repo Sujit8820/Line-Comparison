b42daf682bd84d0ba1efc8d39c70c1ff92e7784e
Sujit8820 <kadamsujit8820@gmail.com>
2022-02-14 14:30:54 +0530

    [Sujit] ADDED: Check Equality of two lines

--- Program.cs
+++ Program.cs
@@ -5,23 +5,24 @@ namespace Line_Comparison
 {
     class program
     {
-        public static void UserInpur()
+        public static double UserInpur()
         {
             Console.WriteLine("Enter first co-ordinate of first point");
             int x1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("\nEnter second co-ordinate of first point");
             int y1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"\n  First point ({x1},{y1})");
+            Console.WriteLine("------------------------------------------");
 
             Console.WriteLine("\nEnter first co-ordinate of second point");
             int x2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("\nEnter second co-ordinate of second point");
             int y2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"\n  Second point ({x1},{y2})\n");
+            Console.WriteLine($"\n  Second point ({x2},{y2})");
+            Console.WriteLine("===========================================");
 
-            double lineLength = CalculateLength(x1,y1,x2,y2);
-            Console.WriteLine("Length of a line: "+lineLength);
-
+            double lineLength = CalculateLength(x1, y1, x2, y2);
+            return lineLength;
         }
         public static double CalculateLength(int x1, int y1, int x2, int y2)
         {
@@ -31,14 +32,24 @@ namespace Line_Comparison
         public static void Main(string[] args)
         {
             Console.WriteLine("Wellcome to Line Comparison Program\n");
+            Console.WriteLine("For first line\n");
+            double firstLineLength = UserInpur();
+            Console.WriteLine("\nFor Second line\n");
+            double secondLineLength = UserInpur();
 
-            UserInpur();
-            
-            
+            Console.WriteLine($"\nFirst line length: {firstLineLength}\nSecond line length: {secondLineLength}\n");
 
+            bool status = firstLineLength.Equals(secondLineLength);
+            if (status == true)
+            {
+                Console.WriteLine("Two lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("Two lines are not equal");
+            }
         }
 
     }
-
 
 }
