1fbebed1a532ebea959850b10e44d82a3eeea61f
Sujit8820 <kadamsujit8820@gmail.com>
2022-02-14 12:18:52 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Line_Comparison
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to Line Comparison Program");
+        }
+
+    }
+
+
+}
