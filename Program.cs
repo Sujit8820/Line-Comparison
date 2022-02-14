045a458ead22befe2f114994eb0f2f8c73fc8c8a
Sujit8820 <kadamsujit8820@gmail.com>
2022-02-14 13:49:36 +0530

    [Sujit] ADDED: Calculate length of a line

--- Program.cs
+++ Program.cs
@@ -5,9 +5,37 @@ namespace Line_Comparison
 {
     class program
     {
+        public static void UserInpur()
+        {
+            Console.WriteLine("Enter first co-ordinate of first point");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nEnter second co-ordinate of first point");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"\n  First point ({x1},{y1})");
+
+            Console.WriteLine("\nEnter first co-ordinate of second point");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nEnter second co-ordinate of second point");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"\n  Second point ({x1},{y2})\n");
+
+            double lineLength = CalculateLength(x1,y1,x2,y2);
+            Console.WriteLine("Length of a line: "+lineLength);
+
+        }
+        public static double CalculateLength(int x1, int y1, int x2, int y2)
+        {
+            double length = Math.Sqrt(Math.Pow((x2 - x1), 2) + (Math.Pow((y2 - y1), 2)));
+            return length;
+        }
         public static void Main(string[] args)
         {
-            Console.WriteLine("Wellcome to Line Comparison Program");
+            Console.WriteLine("Wellcome to Line Comparison Program\n");
+
+            UserInpur();
+            
+            
+
         }
 
     }
