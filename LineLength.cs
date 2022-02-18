591e4cb42ef6d1539592ed39b48b0c37a1f10986
Sujit8820 <kadamsujit8820@gmail.com>
2022-02-18 22:40:46 +0530

    [Sujit] ADDED : Line comparison using OOPS concept

---
+++ LineLength.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Comparison
+{
+    class LineLength
+    {
+        public double GetLength()
+        {
+            Console.WriteLine("Enter first co-ordinate (x1,y1)");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"\n  First point ({x1},{y1})");
+
+            Console.WriteLine("\nEnter second co-ordinates (x2,y2)");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"\n  Second point ({x2},{y2})");
+
+            double length = CalculateLength(x1,y1,x2,y2);
+            return length;
+        }
+        public static double CalculateLength(int x1, int y1, int x2, int y2)
+        {
+            double length = Math.Sqrt(Math.Pow((x2 - x1), 2) + (Math.Pow((y2 - y1), 2)));
+            return length;
+        }
+
+   
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,46 +5,27 @@ namespace Line_Comparison
 {
     class program
     {
-        public static double UserInpur()
-        {
-            Console.WriteLine("Enter first co-ordinate of first point");
-            int x1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("\nEnter second co-ordinate of first point");
-            int y1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"\n  First point ({x1},{y1})");
-            Console.WriteLine("------------------------------------------");
-
-            Console.WriteLine("\nEnter first co-ordinate of second point");
-            int x2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("\nEnter second co-ordinate of second point");
-            int y2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"\n  Second point ({x2},{y2})");
-            Console.WriteLine("===========================================");
-
-            double lineLength = CalculateLength(x1, y1, x2, y2);
-            return lineLength;
-        }
-        public static double CalculateLength(int x1, int y1, int x2, int y2)
-        {
-            double length = Math.Sqrt(Math.Pow((x2 - x1), 2) + (Math.Pow((y2 - y1), 2)));
-            return length;
-        }
+        
+       
         public static void Main(string[] args)
         {
             Console.WriteLine("Wellcome to Line Comparison Program\n");
             Console.WriteLine("For first line\n");
-            double firstLineLength = UserInpur();
+            LineLength lineLength1 = new LineLength();
+            double firstLineLength = lineLength1.GetLength();
+
             Console.WriteLine("\nFor Second line\n");
-            double secondLineLength = UserInpur();
+            LineLength lineLength2 = new LineLength();
+            double secondLineLength = lineLength2.GetLength();
 
             Console.WriteLine($"\nFirst line length: {firstLineLength}\nSecond line length: {secondLineLength}\n");
 
-            int status = (firstLineLength.CompareTo(secondLineLength));
-            if (status == 0)
+            int compare = (firstLineLength.CompareTo(secondLineLength));
+            if (compare == 0)
             {
                 Console.WriteLine("Two lines are equal");
             }
-            else if (status == 1)
+            else if (compare == 1)
             {
                 Console.WriteLine("First line is greater than second line");
             }
